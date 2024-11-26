cbc22731c936e35fb4bb7747fb735efe23446ee1
A2va <leresche.antoine@gmail.com>
2024-11-26 17:20:51 +0100

    Fix message

--- src/main/java/ch/heigvd/dai/utils/Message.java
+++ src/main/java/ch/heigvd/dai/utils/Message.java
@@ -1,5 +1,8 @@
 package ch.heigvd.dai.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public enum Message {
   START("STRT"),
   FLY("FLYY"),
@@ -22,9 +25,10 @@ public enum Message {
     this.message = message;
   }
 
-  public void setData(String data) {
+  public Message setData(String data) {
     if (this == Message.PIPE || this == Message.DATA || this == Message.JOIN) {
       this.data = data;
+      return this;
     }
     throw new IllegalArgumentException(message + " cannot carry data");
   }
@@ -33,13 +37,21 @@ public enum Message {
     return data;
   }
 
+  @Override
+  public String toString() {
+    if (data == null) {
+      return message + EOT;
+    }
+    return message + SEPARATOR + data + EOT;
+  }
+
   public static Message fromString(String message) {
-    String msg = message.substring(0, 3);
+    String msg = message.substring(0, 4);
 
     for (Message m : Message.values()) {
       if (m.message.equals(msg)) {
         if (m == Message.PIPE || m == Message.DATA || m == Message.JOIN) {
-          m.setData(message.substring(3));
+          m.setData(message.substring(4, message.length() - 1));
         }
         return m;
       }
@@ -47,8 +59,17 @@ public enum Message {
     throw new IllegalArgumentException("No constant with text " + message + " found");
   }
 
-  @Override
-  public String toString() {
-    return super.toString() + SEPARATOR + message + EOT;
+  public static String readUntilEOT(BufferedReader reader) throws IOException {
+    StringBuilder response = new StringBuilder();
+    int c;
+
+    while ((c = reader.read()) != -1) {
+      if (c == EOT) {
+        break;
+      }
+      response.append((char) c);
+    }
+
+    return response.toString();
   }
 }
